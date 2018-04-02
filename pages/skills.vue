9d9e246a47bc74f7cab4289c8f19068a67c89482
Nuno Góis <nunogois@iqa.pt>
2018-04-02 14:03:04 +0100

    skills

--- pages/skills.vue
+++ pages/skills.vue
@@ -8,6 +8,7 @@
         <div class="columns is-multiline">                
             <item v-for="item in s_items" :key="item.id" :library="item.library" :text="item.text" :icon="item.icon" :title="item.title" :color="item.color" :content="item.content"/>
         </div>
+        <span v-show="s_items.length === 0">It seems I may not be familiar with that. Please <router-link :to="'/contact'">tell me about it</router-link> so I can research it!</span>
     </div>
   </div>
 </template>
@@ -27,7 +28,7 @@ import item from '~/components/item.vue'
                     icon: 'js-square',
                     title: 'JavaScript',
                     color: '#f7df1e',
-                    content: "In my opinion, JavaScript is the language you <i>need</i> to know in 2018. Yes, it has its weird parts, but you can do pretty much everything you think of with it and a bit of front-end knowledge (HTML, CSS). I use JSON even for non-JavaScript related projects. I've started using JavaScript mostly with jQuery as a DOM manipulator and AJAX, but now I'm using it with Node.js (npm/yarn are great!), Nuxt.js/Vue.js, Socket.io, NativeScript, some experiments on Electron and just plain JavaScript using some new ECMAScript cool features sometimes. TypeScript is great from what I've seen and I intend to dive deeper into it at a later stage."
+                    content: "In my opinion, JavaScript is the language you <i>need</i> to know in 2018. Yes, it has its weird parts, but you can do pretty much everything you think of with it and a bit of front-end knowledge (HTML, CSS). I use JSON even for non-JavaScript related projects. I've started using JavaScript mostly with jQuery as a DOM manipulator and AJAX, but now I'm using it with Node.js with Express, Nuxt.js/Vue.js, Socket.io, NativeScript, some experiments on Electron and just plain JavaScript using some new ECMAScript cool features sometimes. TypeScript is great from what I've seen and I intend to dive deeper into it at a later stage."
                 },
                 {
                     id: 1,
@@ -102,8 +103,16 @@ import item from '~/components/item.vue'
                     library: 'fas',
                     icon: 'gamepad',
                     title: 'Game Development',
-                    color: '#daeaa9',
-                    content: "I've explored several mobile frameworks including PhoneGap/Cordova, Xamarin, Framework7, Jasonette and NativeScript. I've ended up using NativeScript the most as I feel it offers the best solution for native apps using my know-how. For simple websites that are already responsive, I like using tools like PWABuilder."
+                    color: '#1fff3c',
+                    content: "The game engine I'm most familiar with is Unity, that I use along with C# to create simple 2D games and prototypes that are cross-platform. Other than Unity I've experimented with XNA, Unreal Engine, LibGDX and others."
+                },
+                {
+                    id: 11,         
+                    library: 'fas',
+                    icon: 'flask',
+                    title: 'Others / Researching...',
+                    color: '#2399f7',
+                    content: "Other than the skills listed here I'm always looking forward to add to my knowledge! Some of the things I'm interested in currently: Progressive Web Apps, GraphQL, Go, Python, WebAssembly, Blockchain, Phoenix (Elixir), and so on. Know of something that will interest me? <a href=\"/contact\">Tell me about it!</a>"
                 }
             ]
         }
