da2102ce28c9b4b2d3b9e8c3bb34cf4095ffc0b1
yokiharo <yokiharo@gmail.com>
2018-08-03 20:03:52 +0100

    small blog upgrade + webdev_2

---
+++ components/blogItem.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="column is-4">    
+    <div class="card animated fadeIn" v-show="loaded">
+        <a :href="link" class="card-link">
+        <h1 class="card-category" v-html="title"></h1>
+        <h1 class="card-subtitle" v-html="subtitle" :style="{color: color}"></h1>
+        </a>
+        <div class="card-content">            
+            <div class="content" v-html="description"></div>
+        </div>
+    </div>    
+</div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'subtitle', 'color', 'description', 'link'],
+    data () {
+        return {
+            loaded: false
+        }
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.loaded = true;
+        })
+    }
+}
+</script>
+
+<style>
+.card {
+    border-radius: 20px;
+    background-color:transparent;
+    color:white;
+    border:1px solid white;
+    height:100%;
+}
+.card-image {
+    text-align:center;
+    padding-top:20px;
+}
+
+.card-link, .card-link:hover {
+    color: #fff
+}
+
+.card-image > .image {
+    display:inline-block;
+}
+
+.card-category {
+    padding-top:20px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 20px;
+}
+
+.card-subtitle {
+    text-align: center;
+    font-size: 18px;
+}
+
+.card-content {
+    padding-top:0px;
+}
+
+.card-content > .content {
+    padding-top:20px;
+}
+</style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -2,11 +2,47 @@
   <div>    
     <h1 class="page_title">// BLOG</h1>
     <div class="container">
-        <font-awesome-icon :icon="['fas', 'graduation-cap']" style="font-size:56px;" />
+        <font-awesome-icon :icon="['fas', 'graduation-cap']" style="font-size:56px;" class="animated pulse infinite" />
         <span style="font-size:28px;">Learning Series</span>
+        <span> - Click the title of any of the articles below:</span>
         <div class="columns is-multiline" style="padding-top:20px">                
-            <div class="column"><a href="/blog/learning/webdev_1.html">Web Development - 1. Basics</a></div>
+          <blog-item v-for="item in items" :key="item.id" :title="item.title" :subtitle="item.subtitle" :color="item.color" :description="item.description" :link="item.link" />
         </div>
     </div>
   </div>
 </template>
+
+<script>
+import blogItem from '~/components/blogItem.vue'
+
+  export default {
+    data () {
+        return {
+            items: []
+        }
+    },
+    components: {
+      blogItem
+    },
+    mounted: function () {
+        this.items = [ 
+                {
+                    id: 0,
+                    title: 'Web Development',
+                    subtitle: '1. Basics',
+                    color: '#ffe255',
+                    description: "Basic introduction to Web and Web Development. Learn about concepts like Back-end and Front-end.",
+                    link: "/blog/learning/webdev_1.html"
+                },
+                {
+                    id: 1,                                    
+                    title: 'Web Development',
+                    subtitle: '2. New Project',
+                    color: '#52ffff',
+                    description: "Creating a new project from scratch. Learn about Node, VS Code and JSON. Create a basic page with HTML, CSS and JS.",
+                    link: "/blog/learning/webdev_2.html"
+                }
+            ]
+    }
+  }
+</script>
